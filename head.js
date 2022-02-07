93ba6f53f24987c4a7b7aedf7630326791db2d70
tofutigerz <hfu3@ualberta.ca>
2022-02-07 15:07:06 -0700

    finished head function

---
+++ head.js
@@ -0,0 +1,16 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`👍👍Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🤬🤬Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const head = function(arr) {
+  return arr[0];
+};
+
+assertEqual(head([1,2,3]), 1);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([1]), 1);
+assertEqual(head([]), undefined);
