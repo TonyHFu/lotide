46253cdc84f032b4260420f3d16781397439e55a
tofutigerz <hfu3@ualberta.ca>
2022-02-07 23:05:18 -0700

    flatten function implemented

---
+++ flatten.js
@@ -0,0 +1,19 @@
+const flatten = function(arr) {  
+  let ans =[];
+  for (let i = 0; i < arr.length; i++) {
+    let temp;
+    if (Array.isArray(arr[i])) {
+      temp = flatten(arr[i]);
+    } else {
+      temp = arr[i];
+    }
+    // console.log("temp is", temp);
+    ans = ans.concat(temp);
+    // console.log("ans is", ans);
+  }
+
+  // console.log("final ans is", ans);
+  return ans;
+};
+
+// console.log(flatten([1,2,3,[4,5],[6,[7,8]]]));
