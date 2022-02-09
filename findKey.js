bd8f55e98f5f83f0517c10eb1dc8b09257a10726
tofutigerz <hfu3@ualberta.ca>
2022-02-08 19:31:09 -0700

    added findKey function

---
+++ findKey.js
@@ -0,0 +1,69 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`👍👍Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🤬🤬Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKey = function(dict, callback) {
+  const defaultResult = undefined;
+
+  for (let instance in dict) {
+    if (callback(dict[instance])) {
+      return instance;
+    }
+  }
+  return defaultResult;
+};
+
+
+const result1 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2); // => "noma"
+
+assertEqual(result1, "noma");
+
+const result2 = findKey({
+
+}, x => x > 0);
+
+assertEqual(result2, undefined);
+
+const result3 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars > 4);
+
+assertEqual(result3, undefined);
+
+const result4 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => '');
+
+assertEqual(result4, undefined);
+
+const result5 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => 1);
+
+assertEqual(result5, "Blue Hill");
