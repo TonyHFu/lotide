9007821ea02e1753876ce645b1e661e0efdd2cf3
tofutigerz <hfu3@ualberta.ca>
2022-02-10 09:11:10 -0700

    made eqArrays recursive

--- eqObjects.js
+++ eqObjects.js
@@ -6,18 +6,50 @@ const assertEqual = function(actual, expected) {
   }
 };
 const eqArrays = function(array1, array2) {
+  // console.log("array1", array1);
+  // console.log("array2", array2);
   if (array1.length !== array2.length) {
+    // console.log("array lengths not equal");
     return false;
   }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
+  if (array1.length === 0 && array2.length === 0) {
+    // console.log("reached end of arrays");
+    return true;
   }
-  return true;
+  const element1 = array1.shift();
+  const element2 = array2.shift();
+  
+  if (Array.isArray(element1) && Array.isArray(element2)) {
+    // console.log("both first elements are arrays");
+    let arraysEqual = eqArrays(element1, element2);
+    // console.log("arraysEqual", arraysEqual);
+    // console.log(arraysEqual? "first element arrays equal": "first element arrays not equal");
+    return arraysEqual && eqArrays(array1, array2);
+  } else if (Array.isArray(element1) || Array.isArray(element2)) {
+    // console.log("only one first element is array");
+    return false;
+  } else if (typeof element1 === "object" && typeof element2 === "object") {
+    // console.log("both first elements are objects");
+    let objectEqual = eqObjects(element1, element2);
+    // console.log(objectEqual? "first element object equal": "first element object not equal");
+    return objectEqual && eqArrays(array1, array2);
+  } else if (typeof element1 === "object" || typeof element2 === "object") {
+    // console.log("only one first element is object");
+    return false;
+  } else {
+    // console.log("neither elements are objects or arrays");
+    let elementsEqual = element1 === element2;
+    // console.log(elementsEqual? "first element equal": "first element not equal");
+    return elementsEqual && eqArrays(array1, array2);
+  }
+
 };
 
 const eqObjects = function(object1, object2) {
+  // console.log("object1", object1);
+  // console.log("object2", object2)
+  
+  
   let keys1 = Object.keys(object1);
   let keys2 = Object.keys(object2);
   
@@ -48,20 +80,26 @@ const eqObjects = function(object1, object2) {
   // console.log("element2", element2);
   
   if (Array.isArray(element1) && Array.isArray(element2)) {
+    // console.log("both first elements are arrays");
     let arraysEqual = eqArrays(element1, element2);
+    // console.log("arraysEqual", arraysEqual);
     delete object1[firstKey];
     delete object2[firstKey];
     return arraysEqual && eqObjects(object1, object2);
   } else if (Array.isArray(element1) || Array.isArray(element2)) {
+    // console.log("only one first element is array");
     return false;
   } else if (typeof element1 === "object" && typeof element2 === "object") {
+    // console.log("both first elements are objects");
     let objectEqual = eqObjects(element1, element2);
     delete object1[firstKey];
     delete object2[firstKey];
     return objectEqual && eqObjects(object1, object2);
   } else if (typeof element1 === "object" || typeof element2 === "object") {
+    // console.log("only one first element is object");
     return false;
   } else {
+    // console.log("neither elements are objects or arrays");
     let elementsEqual = element1 === element2;
     delete object1[firstKey];
     delete object2[firstKey];
@@ -89,4 +127,6 @@ assertEqual(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), true); // =
 assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), false); // => false
 assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 }), false); // => false
 
-assertEqual(eqObjects({ a: { z: {c: 2, d: 5, e: {x: 9}} }, b: 2 }, { a: { z: {c: 2, d: 5, e: {x: 9}} }, b: 2 }), true); // => true
+assertEqual(eqObjects({ a: { z: {c: 2, t: [1, 2, [3, 4]],d: 5, e: {x: 9}} }, b: 2 }, { a: { z: {c: 2, t: [1, 2, [3, 4]],d: 5, e: {x: 9}} }, b: 2 }), true); // => true
+
+assertEqual(eqObjects({ a: { z: {c: 2, t: [1, 2, [{a: 1, b: [1]}, 4]],d: 5, e: {x: 9}} }, b: 2 }, { a: { z: {c: 2, t: [1, 2, [{a: 1, b: [1]}, 4]],d: 5, e: {x: 9}} }, b: 2 }), true); // => true
