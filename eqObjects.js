8e86f14bd491f2018177f72262ad4c1e5445a7d8
tofutigerz <hfu3@ualberta.ca>
2022-02-10 08:29:24 -0700

    made eqObject.js recursive

--- eqObjects.js
+++ eqObjects.js
@@ -20,24 +20,53 @@ const eqArrays = function(array1, array2) {
 const eqObjects = function(object1, object2) {
   let keys1 = Object.keys(object1);
   let keys2 = Object.keys(object2);
+  
+  // console.log("keys1", keys1);
+  // console.log("keys2", keys2);
+
   if (keys1.length !== keys2.length) {
+    // console.log("key lengths not equal"); 
+    return false;
+  }
+  if (keys1.length === 0 && keys2.length === 0) {
+    // console.log("reached end of object")
+    return true;
+  }
+
+  let firstKey = keys1[0]
+  // console.log("firstKey", firstKey);
+  if (!keys2.includes(firstKey)) {
+    // console.log("key in first not in second");
     return false;
   }
 
-  for (let key of keys1) {
-    // console.log(object1[key]);
-    // console.log(object2[key]);
-    if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
-      if (!(eqArrays(object1[key], object2[key]))) {
-          // console.log("arrays are false");
-          return false;
-      }
-    }
-    else if (object1[key] !== object2[key]) {
-      return false;
-    }
+
+  let element1 = object1[firstKey];
+  let element2 = object2[firstKey];
+
+  // console.log("element1", element1);
+  // console.log("element2", element2);
+  
+  if (Array.isArray(element1) && Array.isArray(element2)) {
+    let arraysEqual = eqArrays(element1, element2);
+    delete object1[firstKey];
+    delete object2[firstKey];
+    return arraysEqual && eqObjects(object1, object2);
+  } else if (Array.isArray(element1) || Array.isArray(element2)) {
+    return false;
+  } else if (typeof element1 === "object" && typeof element2 === "object") {
+    let objectEqual = eqObjects(element1, element2);
+    delete object1[firstKey];
+    delete object2[firstKey];
+    return objectEqual && eqObjects(object1, object2);
+  } else if (typeof element1 === "object" || typeof element2 === "object") {
+    return false;
+  } else {
+    let elementsEqual = element1 === element2;
+    delete object1[firstKey];
+    delete object2[firstKey];
+    return elementsEqual && eqObjects(object1, object2);
   }
-  return true;
 
 };
 
@@ -54,3 +83,10 @@ assertEqual(eqObjects(cd, dc), true); // => true
 
 const cd2 = { c: "1", d: ["2", 3, 4] };
 assertEqual(eqObjects(cd, cd2), false); // => false
+
+assertEqual(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), true); // => true
+
+assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), false); // => false
+assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 }), false); // => false
+
+assertEqual(eqObjects({ a: { z: {c: 2, d: 5, e: {x: 9}} }, b: 2 }, { a: { z: {c: 2, d: 5, e: {x: 9}} }, b: 2 }), true); // => true
